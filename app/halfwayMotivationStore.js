94b2a83a501ab714fefbb8b0a3fbd05226a52b45
Nick Taras <nicktaras@hotmail.co.uk>
2019-01-15 18:33:11 +1100

    Update halfwayMotivationStore.js

--- app/halfwayMotivationStore.js
+++ app/halfwayMotivationStore.js
@@ -1,8 +1,7 @@
 const halfwayMotivationStore = [
-  "Take a breath, <break time='1s'/> we're half way there, in three seconds lets keep going <break time='3s'/>, ok ",
-  "We'll keep going, we're half way there. Ok, next, ",
-  "We'll keep going, because we're awesome. So, next, ",
-  "We'll keep going, because we're awesome and we're already half way through this exercise. Now, ",
+  "You are doing great, lets keep going. Ok, next ",
+  "Keep going, we're half way there. Ok, next, ",
+  "Half way there. So, next, "
 ]
 
 module.exports = halfwayMotivationStore;
