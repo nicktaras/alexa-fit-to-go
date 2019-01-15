49954e42313f42451a11a1b8fcb05795c2b6893c
Nick Taras <nicktaras@hotmail.co.uk>
2019-01-15 18:47:28 +1100

    Update jokeStore.js

--- app/jokeStore.js
+++ app/jokeStore.js
@@ -4,7 +4,8 @@ const jokeStore = [
   'I made the mistake of buying a running machine the other day. I Haven\'t seen it since.',
   'I have to exercise in the morning before my brain figures out what I\'m doing.',
   'Go to the gym at 4:00 in the morning and you\'ve got the whole gym to yourself.',
-  'I don\'t always go to the gym, but when I do, I make sure Facebook knows about it.'
+  'I don\'t always go to the gym, but when I do, I make sure Facebook knows about it.',
+  'One does not simply get ripped in 3 weeks.'
 ]
 
 module.exports = jokeStore;
