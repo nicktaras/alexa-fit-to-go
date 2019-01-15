0beb6e859e217f6320b4c6ac31c13b8a71e5f818
Nick Taras <nicktaras@hotmail.co.uk>
2019-01-15 18:12:32 +1100

    Update chitChatExerciseStore.js

--- app/chitChatExerciseStore.js
+++ app/chitChatExerciseStore.js
@@ -6,7 +6,104 @@ const chitChatExerciseStore = {
     "did you know that jogging helps combat stress and improves mental fitness. ",
     "there's nothing better than a short jog to ease some stress. ",
     "there's nothing better than getting some fresh air, putting some music on and jogging. ",
+  ],
+  "RUN" : [
+    "did you know that running helps to strengthen muscles. ",
+    "did you know that running helps combat stress and improves mental fitness. ",
+    "there's nothing better than a short run to ease some stress. ",
+    "did you know that running can significantly improve physical and mental health ",
+    "did you know that running can help alleviate symptoms of depression ",
+    "there's nothing better than getting some fresh air, putting some music on and having a run. ",
+  ],
+  "SWIM" : [
+    "did you know that swimming helps to build endurance ",
+    "did you know that swimming helps to build muscle strength and cardiovascular fitness. ",
+    "did you know that swimming helps to maintain a healthy weight ",
+    "did you know that swimming helps you to maintain a healthy heart and lungs. ",
+    "did you know that swimming helps to tone muscles and build strength. "
+    "did you know that swimming helps to provide an all-over body workout, ", 
+    "did you know that nearly all of your muscles are used during swimming. "
+  ],
+  "SOCCER" : [
+    "did you know that soccer is a great sport for burning fat. It works the muscles and your heart in different ways. ",
+    "did you know that soccer can help lower body fat and improves muscle tone ",
+    "did you know that soccer can help build strength, flexibility and endurance "
+  ],
+  "NETBALL": [
+    "did you know that the jumping involved in Netball and other physical demands help develop and improve bone health ",
+  ]
+  "BASKETBALL": [
+    "did you know that the jumping involved in Basketball and other physical demands help develop and improve bone health ",
+    "did you know that Basketball can help develop your lower back, neck, and core muscles "
+  ],
+  "GOLF": [
+    "did you know that golf can help your heart ", 
+    "did you know that by playing golf you can walk an average of five to seven kilometres. If you don't choose to ride the buggy that is ",
+    "did you know that golf can help improve muscle tone and endurance "
+  ],
+  "TENNIS": [
+    "Tennis requires you to be creative, using tactical thinking which is great for the brain. ", 
+    "Playing Tennis will help you to build up better coordination. ",
+    "Tennis helps to exericse the legs, shoulders, arms and many other areas of your body, its a great body workout. ",
+    "did you know that a singles match of Tennis can help you to burn around 600 calories. "
+  ],
+  "WEIGHTS": [
+    "did you know that weight training can be an effective way to tone your body and lose weight. ",
+    "did you know that weight training can help build muscle mass which in turn will help you lose fat. "
+  ],
+  "CRICKET": [
+    "did you know that cricket helps hand and eye coordination. "
+  ],
+  "BOX": [
+    "did you know that boxing helps hand and eye coordination. "
+  ],
+  "KAYAK": [
+    "Kayaking is a great, low impact sport. Theres never a better excuse to get out there and enjoy the views. ",
+    "did you know that Kayaking can improve your aerobic fitness, strength and flexibility. "
+  ],
+  "SURF: [
+    "Surfers spend a lot of time paddling, this helps you to build stamina and power for when that perfect wave comes along ",
+    "did you know that surfing helps you to gain muscle strength and endurance. ",
+    "did you know that having good core strength will help you to handle the stronger waves out there. Take care out there today. "
+  ],
+  "FRISBEE": [
+    "did you know that Frisbee helps you to build up your endurance when running. ",
+    "did you know that Frisbee helps build your agility, with those quick turns, pivots, throws and jumps. Enjoy yourself, I would come too if I could. ",
+    "did you know that Frisbee helps to boost mood, through team work and exercise in the game. "
+  ],
+  "CYCLING": [
+    "did you know that Cycling is mainly an aerobic activity, which means that your heart, blood vessels and lungs all get a good workout. ",
+    "did you know that cycling can increase your overall fitness level, so enjoy the ride! "
+  ],
+  "GARDENING": [
+    "did you know that gardening can help lower blood pressure and cholesterol or prevent diabetes, heart disease, depression, and osteoporosis when practiced on a regular basis. ",
+    "Gardening can help give all your muscle groups a work out. This including your arms, legs, buttocks, stomach, neck and back. All this whilst helping the garden to prosper. ",
+    "did you know that Gardening can help you to burn around 150 to 300 calories within 45 minutes. ",
+    "Some people claim gardening is the ultimate workout, go you and good choice! I hope you enjoy it out there today. "
+  ],
+  "YOGA": [
+    "did you know that yoga can help increase your all round fitness. ",
+    "did you know that yoga can you to become more mindfull, happy and reduce stress. I hope it does for you today. "
+  ],
+  "PADDLE_BOARD": [
+    "did you know that paddle boarding can help increase your balance. ",
+  ],
+  "HIKING": [
+    "did you know that hiking can help build strength in your glutes, quadriceps, hamstrings, and the muscles in your hips and lower legs. "
+  ],
+  "ROCK_CLIMBING": [
+    "did you know that rock climbing can help build strength in your glutes, quadriceps, hamstrings, and the muscles in your hips and lower legs. "
+  ],
+  "CROSS_TRAINING": [
+    "did you know that rock climbing can help build strength in your glutes, quadriceps, hamstrings, and the muscles in your hips and lower legs. "
+  ],
+  "HORSE_RIDING: [
+    "did you know that horse riding can be good for your lower back. "
+  ],
+  "SKATING": [
+    "did you know that skating helps to improve coordination and balance. "
   ]
 }
+
 
 module.exports = chitChatExerciseStore;
